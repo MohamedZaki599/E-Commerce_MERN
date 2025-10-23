09dec14c5d63112f5e25b4908577cc1767e18657
MohamedZaki599 <59538937+MohamedZaki599@users.noreply.github.com>
2025-10-23 18:49:51 +0300

    Scaffolding the frontend project + Updating the folder structure + install MUI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function App(): React.JSX.Element {
+  return (
+    <div>
+      <a href="https://vite.dev" target="_blank" rel="noreferrer">
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+      </a>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button type="button">count is 0</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  )
+}
+
+
